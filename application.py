1624357f9ac56b87108de278ba1f03e29f577d92
Samuel Vásquez <vasquezsamuel643@gmail.com>
2021-09-19 23:38:26 -0600

    subida de datos a la BD y conexion de este

--- application.py
+++ application.py
@@ -1,9 +1,12 @@
+from logging import error
 import os
 
-from flask import Flask, session, render_template
+from flask import Flask,flash, session, render_template, request, redirect
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from werkzeug.exceptions import default_exceptions
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
@@ -20,14 +23,62 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-@app.route("/", methods = ["GET", "POST"])
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+
+    if request.method=="POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        #nos aseguramos que haya llenado el username 
+        if not username:
+            return render_template("error.html", 403)
+
+        elif not password:
+            return render_template("error.html", 403)
+
+        consulta = db.execute("SELECT * FROM usuarios WHERE username = :username", username=request.form.get("username"))
+        
+        if len(consulta) != 1:
+            return render_template("error.html")
+        if not check_password_hash(consulta[0][password], password):
+            return render_template("error.html")
+        
+        session["id_usuario"] = consulta[0]["id_usuario"]
+
+        return redirect("/")
+    else:
+         return render_template("login.html")
+
+@app.route("/register", methods=["GET","POST"])
+def register():
+
+    session.clear()
+
+    if request.method == "POST":
+        usser = request.form.get("username")
+        contra = request.form.get("password")
+
+        if not usser:
+            return render_template("error.html")
+
+        if not contra:
+            return render_template("error.html")
+
+        consulta = db.execute("SELECT * FROM usuarios WHERE username LIKE :usser", usser)
+
+        if len(consulta) == 1:
+            return "usuario tomado"
+
+        contra = generate_password_hash(contra)
+        nuevo_usuario = db.execute("INSERT INTO usuarios(password, username) VALUES(:password, :username) ", username=usser, password=contra)
+
+        session["id_usuario"] = nuevo_usuario
+        flash("Registrado Exitosamente!")
+
+        return redirect(url_for("index"))
+    else:
+        return render_template("register.html")
+
+@app.route("/")
 def index():
-    if not request.form.get("username")
-        return apology("must provide username", 403)
-
-    
-    return render_template("login.html")
-
-@app.route("/register")
-def crear_usuario():
-    return render_template("register.html")
+    return render_template("index.html")
--- import.py
+++ import.py
@@ -3,10 +3,9 @@ import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from dotenv import load_dotenv
 
-os.environ['DATABASE_URL']='postgres://egtusoaqicdabv:1d8d78b9a957560fafc4d26c218390929934c01a463c6e05e14e9b0c8e7022c8@ec2-52-2-118-38.compute-1.amazonaws.com:5432/d174ofjmjit41'
-os.environ['FLASK_DEBUG']='1'
-os.environ['FLASK_APP']='application.py'
+load_dotenv()
 
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
@@ -15,7 +14,7 @@ def main():
     f = open("books.csv")
     reader = csv.reader(f)
     for isbn, title, author, year in reader:
-        db.execute("INSERT INTO flights (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
+        db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
                     {"isbn": isbn, "title": title, "author": author, "year": year})
         print(f"Libros agregado isbn {isbn} title {title} author {author} year {year} ")
     db.commit()
